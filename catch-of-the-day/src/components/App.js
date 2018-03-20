a3eab8361146783a0e40659119c4ecd08a942325
tata <tatasrinath00@gmail.com>
2018-03-20 17:10:16 +0530

    Completed till 16th lesson

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -26,6 +26,15 @@ class App extends React.Component {
         this.setState({fishes: sampleFishes});
     };
 
+    addToOrder = (key) => {
+        // 1. take a copy of state
+        const order  = {...this.state.order};
+        // 2. Either add to the order or update the numbrt in  the order
+        order[key] = order[key] + 1 || 1 ;
+        // 3. Call setState to update our state object
+        this.setState({order: order });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -33,11 +42,16 @@ class App extends React.Component {
                     <Header  tagline="Is cool" age={500} cool={true} quote={getFunName()}/>
                     <ul className="fishes">
                         {Object.keys(this.state.fishes).map(key => 
-                            <Fish key={key} details={this.state.fishes[key]}/>
+                            <Fish 
+                            key={key}
+                            index = {key} 
+                            details={this.state.fishes[key]} 
+                            addToOrder={this.addToOrder}
+                        />
                         )}
                     </ul>
                 </div>
-                <Order />
+                <Order fishes={this.state.fishes} order={this.state.order}/>
                 <Inventory 
                     addFish={this.addFish} 
                     loadSampleFishes={this.loadSampleFishes}
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -3,8 +3,8 @@ import React from 'react';
 class Order extends React.Component {
     render() {
         return(
-            <div className="order">
-                Order
+            <div className="order-wrap" >
+                <h2>Order</h2>
             </div>
         );
     }
